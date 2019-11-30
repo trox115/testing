f29c909b72cb9f49aa86c1024300ee8f6c2be61c
trox115 <tofernandes67@gmail.com>
2019-11-30 00:02:03 +0000

    function returning object

---
+++ array/array.js
@@ -0,0 +1,20 @@
+function analyze(array) {
+  const obj = {};
+  const sum = array.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
+  const maximum = array.reduce((first, last) => {
+    if (first >= last) {
+      return first;
+    }
+    return last;
+  });
+  const min = array.reduce((first, last) => (first <= last ? first : last));
+  obj.average = sum / array.length;
+  obj.max = maximum;
+  obj.min = min;
+  obj.length = array.length;
+  return obj;
+}
+
+export default analyze;
